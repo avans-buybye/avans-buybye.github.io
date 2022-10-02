f7abe191b94b1d1e72c8a1d2d3fba100e7f023dd
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2022-10-02 20:27:45 +0200

    faal icon click navbar

--- ferry/script.js
+++ ferry/script.js
@@ -1,13 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-const klik = document
-  .getElementsByClassName("fa fa-bars")
-  .addEventListener("click", myFunction);
-
-function myFunction() {
+function myIcon() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "navflex") {
+  if (x.className === "navbtns") {
     x.className += " responsive";
   } else {
-    x.className = "navflex";
+    x.className = "navbtns";
   }
 }
