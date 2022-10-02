a51ac63d1df3995f784b398f001bdcda2a132abd
Paul van der Linden <paul@jobport.nl>
2022-10-02 22:41:32 +0200

    Updated main menu on mobile
    + Correct <script> tag in head
    + Changed a tag to button tag to call function
    + Hide menu by default
    + Hide/show button (.toggleNav) on min/max-width: 600px;
    + Used id from div around a tags (#myTopnav) to hide/show links instead of a tags (.navbtns)
    + Made a tags align left + 100%
    - Removed overflow somewhere

--- ferry/script.js
+++ ferry/script.js
@@ -1,6 +1,8 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myIcon() {
   var x = document.getElementById("myTopnav");
+  console.log(x.className);
+
   if (x.className === "navbtns") {
     x.className += " responsive";
   } else {
