bcedaa44ef3528a714e16a8e3322b3f1113af933
Ferry Krooswijk <88162108+appfer@users.noreply.github.com>
2022-10-05 22:23:56 +0200

    vorderingen pop up

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,26 @@ function myIcon() {
     x.className = "navbtns";
   }
 }
+/* popup onclick button contact*/
+document.getElementById("contactbutton").click(function () {
+  this.speedoPopup({
+    width: 550,
+    height: 265,
+    useFrame: TRUE,
+    href: "#contactpop",
+  });
+});
+
+/*const openBtn = document.getElementById("contactbutton");
+
+openBtn.addEventListener("click", () => {
+  contactpop.classList.add("show");
+});
+
+setTimeout(() => {
+  modalMenu.classList.add("show");
+}, 3000);
+
+modalBtn.addEventListener("click", () => {
+  modalMenu.classList.remove("show");
+});*/
