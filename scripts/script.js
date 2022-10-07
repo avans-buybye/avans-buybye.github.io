2593e7d1c1149fcab37d355c975c6ffc894695ed
Paul van der Linden <paul@jobport.nl>
2022-10-07 12:09:04 +0200

    Fixed contact button
    Index.html
    + Fixed file paths (/ instead of \)
    + Added JavaScript script tag to head instead of body
    + Fixed comment being code instead
    + Added button instead of link with no URL
    
    Script
    + Added for loop for adding in-line style
    
    CSS:
    + Added styling for button

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,30 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myIcon() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById('myTopnav');
   console.log(x.className);
 
-  if (x.className === "navbtns") {
-    x.className += " responsive";
+  if (x.className === 'navbtns') {
+    x.className += ' responsive';
   } else {
-    x.className = "navbtns";
+    x.className = 'navbtns';
   }
 }
 //document.getElementById("contactbutton").onclick = function showContact() {
 //document.getElementById("contactpop").style.display = "";
 //};
-/* popup onclick button contact*/
+
+/* Popup onclick button contact */
 function showContact() {
-  var popup = document.getElementById("contactpop");
-  popup.classList.remove("none");
+  var popup = document.getElementById('contactpop'); // Element ophalen voor in-line style
+
+  // popup.classList.remove("none"); // Dit werkt dus niet, omdat je geen toegang hebt tot de css file
+
+  if (!popup.style.display) {
+    popup.style.display = 'initial';
+  } else {
+    document.lo;
+    popup.style.removeProperty('display');
+  }
 }
 
 /*const openBtn = document.getElementById("contactbutton");
