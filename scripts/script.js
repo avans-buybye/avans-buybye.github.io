c02988da94be5d75611c13df1b70d8c0fa0c1bdd
Ferry Krooswijk <88162108+appfer@users.noreply.github.com>
2022-10-08 15:05:20 +0200

    pop ups

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,22 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* responsive menu voor kleine schermen */
 function myIcon() {
-  var x = document.getElementById('myTopnav');
+  var x = document.getElementById("myTopnav");
   console.log(x.className);
 
-  if (x.className === 'navbtns') {
-    x.className += ' responsive';
+  if (x.className === "navbtns") {
+    x.className += " responsive";
   } else {
-    x.className = 'navbtns';
+    x.className = "navbtns";
   }
 }
-//document.getElementById("contactbutton").onclick = function showContact() {
-//document.getElementById("contactpop").style.display = "";
-//};
 
 /* Popup onclick button contact */
 function showContact() {
-  var popup = document.getElementById('contactpop'); // Element ophalen voor in-line style
-
-  // popup.classList.remove("none"); // Dit werkt dus niet, omdat je geen toegang hebt tot de css file
+  var popup = document.getElementById("contactpop"); // Element ophalen voor in-line style
 
   if (!popup.style.display) {
-    popup.style.display = 'initial';
+    popup.style.display = "initial";
   } else {
-    popup.style.removeProperty('display');
+    popup.style.removeProperty("display");
   }
-}
-
-/* Popup onclick button contact with Class list toggle */
-function showContact2() {
-  document.getElementById('contactpop').classList.toggle('show-popup');
 }
